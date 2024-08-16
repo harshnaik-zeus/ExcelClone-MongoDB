3a13ebe6b07a648bc2fe1f313d396173e63a3b79
Harsh Naik <harsh.naik@zeuslearning.com>
2024-08-16 11:07:37 +0530

    rabbitmq added

--- Backend/Services/Consumer.cs
+++ Backend/Services/Consumer.cs
@@ -1,27 +1,62 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Data.SqlClient;
 using System.Text;
 
 public class ConsumerService
 {
     private readonly IModel _channel;
+    private readonly string _connectionString;
 
-    public ConsumerService(IModel channel)
+    public ConsumerService(IModel channel, string connectionString)
     {
         _channel = channel;
+        _connectionString = connectionString;
     }
 
     public void StartConsuming()
     {
-
         var consumer = new EventingBasicConsumer(_channel);
         consumer.Received += (model, ea) =>
         {
             var body = ea.Body.ToArray();
             var message = Encoding.UTF8.GetString(body);
+            var lines = message.Split('\n');
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                foreach (var line in lines)
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        var values = line.Split(',');
+
+                        // Construct the query to insert into all 14 columns
+                        var query = @"INSERT INTO employeeinfos 
+                                      (Column1, Column2, Column3, Column4, Column5, Column6, 
+                                       Column7, Column8, Column9, Column10, Column11, Column12, 
+                                       Column13, Column14) 
+                                      VALUES (@value1, @value2, @value3, @value4, @value5, @value6, 
+                                              @value7, @value8, @value9, @value10, @value11, @value12, 
+                                              @value13, @value14)";
+
+                        using (var command = new SqlCommand(query, connection))
+                        {
+                            // Add parameters for each of the 14 columns
+                            for (int i = 0; i < 14; i++)
+                            {
+                                command.Parameters.AddWithValue($"@value{i + 1}", values.Length > i ? values[i] : (object)DBNull.Value);
+                            }
+
+                            // Execute the query
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
         };
 
         _channel.BasicConsume(queue: "csv_queue", autoAck: true, consumer: consumer);
     }
-
 }
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0d7be4b0b953fd589f663d74a700e98c24f66f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05975d471de1030a202602132206c31b87af4d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
